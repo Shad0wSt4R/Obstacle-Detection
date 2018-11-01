03a48330d9a60414a87b30282d108b29f68153a8
Plytonics <madjosh7@gmail.com>
2018-10-31 22:16:25 -0400

    Add files via upload
    new with comments

--- ImageRec.py
+++ ImageRec.py
@@ -2,19 +2,22 @@ import cv2
 import numpy as np
 MIN_MATCH_COUNT=30
 
+'''feature extractor  - sift = s and accurate or surf = f and not as accurate'''
 detector=cv2.SIFT()
-
+'''feature matcher'''
 FLANN_INDEX_KDITREE=0
 flannParam=dict(algorithm=FLANN_INDEX_KDITREE,tree=5)
 flann=cv2.FlannBasedMatcher(flannParam,{})
 
+'''location of the images to be tested'''
 trainImg=cv2.imread("Images/TrainImg.jpg",0)
 trainKP,trainDesc=detector.detectAndCompute(trainImg,None)
 
+'''capture frame and convert to grayscale'''
 cam=cv2.VideoCapture(0)
 while True:
-    ret, QueryImgBGR=cam.read()
-    QueryImg=cv2.cvtColor(QueryImgBGR,cv2.COLOR_BGR2GRAY)
+    ret, QueryImgRGB=cam.read()
+    QueryImg=cv2.cvtColor(QueryImgRGB,cv2.COLOR_RGB2GRAY)
     queryKP,queryDesc=detector.detectAndCompute(QueryImg,None)
     matches=flann.knnMatch(queryDesc,trainDesc,k=2)
 
@@ -35,8 +38,8 @@ while True:
         queryBorder=cv2.perspectiveTransform(trainBorder,H)
         cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
     else:
-        print "Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT)
-    cv2.imshow('result',QueryImgBGR)
+        print ("Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT))
+    cv2.imshow('result',QueryImgRGB)
     if cv2.waitKey(10)==ord('q'):
         break
 cam.release()
