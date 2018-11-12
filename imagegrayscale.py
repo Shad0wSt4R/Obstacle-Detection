6013dcc33be1fd99f24e1f6c18ab001c8e599099
Shad0wSt4R <hochmuth.marley@gmail.com>
2018-11-12 14:41:48 -0500

    Modularized for training and testing

--- imagegrayscale.py
+++ imagegrayscale.py
@@ -1,16 +1,38 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('driving_vids/Downtown.mp4')
+def training(){
+}
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
+def testing(){
+    cap = cv2.VideoCapture('driving_vids/Downtown.mp4')
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
 
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+}
+
+def main(){
+    print("Welcome to the Obstacle Detection Program!\n")
+    choice = input("What you you like to do?\n\n1) Train\n2) Test")
+
+    if(choice == 1){
+        training()
+    }
+    elif(choice == 2){
+        testing()
+    }
+    else{
+    }
+
+}
+
+main()
