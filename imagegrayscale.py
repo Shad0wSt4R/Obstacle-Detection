eeb688dc2dcc696781ea00d9d15552426ffba3d0
Shad0wSt4R <hochmuth.marley@gmail.com>
2018-11-12 19:01:41 -0500

    cleaned up a bit

--- imagegrayscale.py
+++ imagegrayscale.py
@@ -1,29 +1,33 @@
 import numpy as np
 import cv2
-import keras
-from keras import backend as K
+#import keras
+#from keras import backend as K
 
 
-def training(){
-}
+def training():
+    for i in range(7481)
+        print(i)
 
-def testing(){
+
+def testing():
     cap = cv2.VideoCapture('driving_vids/Downtown.mp4')
 
     while(cap.isOpened()):
         ret, frame = cap.read()
 
+        # Makes the video grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # Shows grayscaled video
         cv2.imshow('frame',gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
-}
 
-def main(){
+
+def main():
     print("Welcome to the Obstacle Detection Program!\n")
     choice = input("What you you like to do?\n\n1) Train\n2) Test")
 
@@ -36,6 +40,5 @@ def main(){
     else{
     }
 
-}
 
 main()
