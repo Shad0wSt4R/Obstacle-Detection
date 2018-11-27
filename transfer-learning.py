ac0c745390fb17b303f119bb53ea0bd39008bcfe
Shad0wSt4R <hochmuth.marley@gmail.com>
2018-11-27 15:21:48 -0500

    More edits

--- transfer-learning.py
+++ transfer-learning.py
@@ -18,7 +18,7 @@ x=GlobalAveragePooling2D()(x)
 x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
 x=Dense(1024,activation='relu')(x) #dense layer 2
 x=Dense(512,activation='relu')(x) #dense layer 3
-preds=Dense(120,activation='softmax')(x) #final layer with softmax activation
+preds=Dense(2,activation='softmax')(x) #final layer with softmax activation
 
 model = Model(inputs=base_model.input,outputs=preds)
 
@@ -36,16 +36,16 @@ for layer in model.layers[20:]:
 
 train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input)
 
-train_genrator=train_datagen.flow_from_directory('path-to-the-main-data-folder',
+train_generator=train_datagen.flow_from_directory('/home/marlena/Obstacle-Detection/training/',
                                                 target_size=(224,224),
                                                 color_mode='rgb',
                                                 batch_size=32,
                                                 class_mode='categorical',
                                                 shuffle=True)
 
-model.compile(optimizer='Adam', loss='categorical_crossentcopy',metrics=['accuracy'])
+model.compile(optimizer='Adam', loss='categorical_crossentropy',metrics=['accuracy'])
 
-step_size_train=train_generator.n/train_generator.batch_size
+step_size_train=train_generator.n//train_generator.batch_size
 model.fit_generator(generator=train_generator,steps_per_epoch=step_size_train, epochs=10)
 
 model.save("model.h5")
